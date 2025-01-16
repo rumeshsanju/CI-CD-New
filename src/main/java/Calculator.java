7c65ee5c9643eaa62c93743e55e95b7111540ddf
rumeshsanju <rumeshrajapaksha1998@gmail.com>
2025-01-17 01:33:26 +0530

    add java files

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,28 @@
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return (double) a / b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Addition: " + calc.add(5, 3));
+        System.out.println("Subtraction: " + calc.subtract(5, 3));
+        System.out.println("Multiplication: " + calc.multiply(5, 3));
+        System.out.println("Division: " + calc.divide(5, 3));
+    }
+}
---
+++ src/test/java/TestCiCd.java
@@ -0,0 +1,27 @@
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class TestCiCd {
+    @Test
+    public void testAdd() {
+        Calculator calc = new Calculator();
+        assertEquals(calc.add(5, 3), 8);
+    }
+
+    @Test
+    public void testSubtract(){Calculator calc = new Calculator();
+        assertEquals(calc.subtract(5, 2), 3);
+    }
+
+    @Test
+    public void testMultiply(){
+        Calculator calc = new Calculator();
+        assertEquals(calc.multiply(6, 5), 30);
+    }
+
+    @Test(expectedExceptions = ArithmeticException.class)
+    public void testDivideByZero() {
+        Calculator calc = new Calculator();
+        calc.divide(5, 0);
+    }
+}
